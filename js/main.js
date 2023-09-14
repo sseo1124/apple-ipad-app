4b393833055bc31113c3fa725e502cd388221bb5
sanghyuk <sanghyuk@sanghyukui-Macmini.local>
2023-09-14 14:47:51 +0900

    header-search's script

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ function showBasket() {
 function hideBasket() {
   basketEl.classList.remove('show')
 }
+
+// 검색!
+const headerEl = document.querySelector('header')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')  //document.docummentElement == html 태그
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+}
